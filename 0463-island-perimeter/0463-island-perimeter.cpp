899bfbbbc761204017b3521eae33c4ea206fc140
Eshwar.Lade <eshwar341arjun@gmail.com>
2025-10-03 11:38:34 +0530

    Time: 15 ms (25.59%), Space: 105.8 MB (17.16%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,47 +1,39 @@
 class Solution {
-private:
-    int dfs(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& vis) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vis[row][col] = 1;
-
-        int delRow[4] = {-1, 0, 1, 0};
-        int delCol[4] = {0, 1, 0, -1};
-
-        int perimeter = 0;
-
-        for (int d = 0; d < 4; d++) {
-            int newRow = row + delRow[d];
-            int newCol = col + delCol[d];
-
-            if (newRow < 0 || newRow >= m || newCol < 0 || newCol >= n) {
-                perimeter++;
-            }
-            else if (grid[newRow][newCol] == 0) {
-                perimeter++;
-            }
-            else if (!vis[newRow][newCol]) {
-                perimeter += dfs(newRow, newCol, grid, vis);
+    private:
+    int dfs(int row,int col,vector<vector<int>>&grid,vector<vector<int>>&vis){
+        int n=grid.size();
+        int m=grid[0].size();
+        vis[row][col]=1;
+        int drow[4]={-1,0,+1,0};
+        int dcol[4]={0,+1,0,-1};
+        int perimeter=0;
+        for(int d=0;d<4;d++){
+            int nrow=row+drow[d];
+            int ncol=col+dcol[d];
+            if(nrow<0||nrow>=n||ncol<0||ncol>=m){
+                 perimeter++;
+            }else if(grid[nrow][ncol]==0){
+                  perimeter++;
+            } else if (!vis[nrow][ncol]){
+                perimeter+=dfs(nrow,ncol,grid,vis);
             }
         }
-
         return perimeter;
     }
-
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    return dfs(i, j, grid, vis);
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    return dfs(i,j,grid,vis);
                 }
             }
         }
-
         return 0;
+
+        
     }
 };
