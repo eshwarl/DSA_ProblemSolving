db1d324d161769a5884d65efa3c64787be937f80
ESHWAR LADE <eshwar341arjun@gmail.com>
2026-07-02 00:07:44 +0530

    Time: 0 ms (100%), Space: 11 MB (81.26%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        sort(costs.begin(), costs.end(), [](vector<int>& a, vector<int>& b) {
+            return (a[1] - a[0]) < (b[1] - b[0]);
+        });
+
+        int n = costs.size();
+        int ans = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            ans += costs[i][1];     
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            ans += costs[i][0];     
+        }
+
+        return ans;
+    }
+};
