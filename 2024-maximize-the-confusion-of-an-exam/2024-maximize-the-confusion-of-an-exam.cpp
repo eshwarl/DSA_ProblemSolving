dc103204623e7fad489977401e2de4bd31c4a484
ESHWAR LADE <eshwar341arjun@gmail.com>
2026-09-11 00:00:43 +0530

    Time: 0 ms (100%), Space: 12.6 MB (54.06%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
-        int l=0,r=0;
-        int maxlen=0;
-        int maxfreq=0;
-        int hash[26]={0};
-        while(r<answerKey.size()){
-            hash[answerKey[r]-'A']++;
-            maxfreq=max(maxfreq,hash[answerKey[r]-'A']);
+        int l = 0;
+        int t = 0, f = 0;
+        int ans = 0;
 
-            if((r-l+1)-maxfreq>k){
-                hash[answerKey[l]-'A']--;
+        for (int r = 0; r < answerKey.size(); r++) {
+
+            if (answerKey[r] == 'T')
+                t++;
+            else
+                f++;
+
+          
+            while (min(t, f) > k) {
+
+                if (answerKey[l] == 'T')
+                    t--;
+                else
+                    f--;
+
                 l++;
             }
-            maxlen=max(maxlen,r-l+1);
-            r++;
 
-
+            ans = max(ans, r - l + 1);
         }
-        return maxlen;
+
+        return ans;
     }
 };
